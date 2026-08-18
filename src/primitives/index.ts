752abf634322f6a7899c0bfa9fee13c16634d81c
Kevni92 <mail@kevinkrone.de>
2026-08-18 16:56:21 +0200

    feat: export data table primitive (#24)

--- src/primitives/index.ts
+++ src/primitives/index.ts
@@ -5,3 +5,17 @@ export { default as KeyValueRow } from './KeyValueRow.vue'
 export { default as StatValue } from './StatValue.vue'
 export { default as SimpleTable } from './SimpleTable.vue'
 export type { SimpleTableAlign, SimpleTableColumn } from './simple-table'
+export { default as DataTable } from './DataTable.vue'
+export {
+  filterDataTableRows,
+  nextDataTableSort,
+  processDataTableRows,
+  sortDataTableRows,
+  visibleDataTableColumns,
+} from './data-table'
+export type {
+  DataTableColumn,
+  DataTableRowId,
+  DataTableSort,
+  DataTableSortDirection,
+} from './data-table'
