5d2822ed148de1cab88da5f14ed54bb8db6bcd33
Kevni92 <mail@kevinkrone.de>
2026-08-18 16:42:52 +0200

    test: add key value primitive fixture (#22)

---
+++ tests/fixtures/KeyValueFixture.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import InfoPopover from '../../src/primitives/InfoPopover.vue'
+import KeyValueGroup from '../../src/primitives/KeyValueGroup.vue'
+import KeyValueRow from '../../src/primitives/KeyValueRow.vue'
+import StatValue from '../../src/primitives/StatValue.vue'
+</script>
+
+<template>
+  <KeyValueGroup compact :columns="2" label="Facility statistics">
+    <KeyValueRow>
+      <template #label>
+        <InfoPopover label="Efficiency explanation">
+          <template #trigger>Efficiency</template>
+          Base output multiplied by staffing.
+        </InfoPopover>
+      </template>
+      <template #value>
+        <StatValue :value="84" unit="%" tone="success" label="Efficiency 84 percent" />
+      </template>
+    </KeyValueRow>
+
+    <KeyValueRow label="Maintenance">
+      <template #value>
+        <button type="button" data-test="maintenance-action">Scheduled</button>
+      </template>
+    </KeyValueRow>
+  </KeyValueGroup>
+</template>
