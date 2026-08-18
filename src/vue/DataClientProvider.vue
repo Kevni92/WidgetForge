ae59b93508314f626cdf79632e8ce232389c9b10
Kevni92 <mail@kevinkrone.de>
2026-08-18 16:09:02 +0200

    feat: provide data client to widget trees (#17)

---
+++ src/vue/DataClientProvider.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import type { DataClient } from '../data/data-client'
+import { provideDataClient } from './data-context'
+
+const props = defineProps<{ client: DataClient }>()
+provideDataClient(props.client)
+</script>
+
+<template>
+  <slot />
+</template>
