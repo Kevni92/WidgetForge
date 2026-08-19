219445221fa2441da2aeeac4062028e1b0495a63
Kevni92 <mail@kevinkrone.de>
2026-08-19 11:29:43 +0200

    Provide persistent widget view state in playground

--- playground/src/main.ts
+++ playground/src/main.ts
@@ -1,8 +1,11 @@
 import 'widgetforge/style.css'
 
-import { createApp } from 'vue'
+import { createApp, defineComponent, h, markRaw } from 'vue'
+import { WidgetViewStateProvider, createLocalStorageWidgetViewStateStorage, createWidgetViewStateStore } from 'widgetforge'
 
 import App from './App.vue'
 import './style.css'
 
-createApp(App).mount('#app')
+const viewStateStore = markRaw(createWidgetViewStateStore(createLocalStorageWidgetViewStateStorage(window.localStorage, 'widgetforge.playground.widget-view-state.v1')))
+const Root = defineComponent({ setup: () => () => h(WidgetViewStateProvider, { store: viewStateStore }, () => h(App)) })
+createApp(Root).mount('#app')
