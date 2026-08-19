fdf092a90a228e5adaa092e4eaab669a3e69eced
Kevni92 <mail@kevinkrone.de>
2026-08-19 12:01:47 +0200

    Provide shared selection store in playground root

--- playground/src/main.ts
+++ playground/src/main.ts
@@ -1,11 +1,20 @@
 import 'widgetforge/style.css'
 
 import { createApp, defineComponent, h, markRaw } from 'vue'
-import { WidgetViewStateProvider, createLocalStorageWidgetViewStateStorage, createWidgetViewStateStore } from 'widgetforge'
+import {
+  SelectionProvider,
+  WidgetViewStateProvider,
+  createLocalStorageWidgetViewStateStorage,
+  createSelectionStore,
+  createWidgetViewStateStore,
+} from 'widgetforge'
 
 import App from './App.vue'
 import './style.css'
 
 const viewStateStore = markRaw(createWidgetViewStateStore(createLocalStorageWidgetViewStateStorage(window.localStorage, 'widgetforge.playground.widget-view-state.v1')))
-const Root = defineComponent({ setup: () => () => h(WidgetViewStateProvider, { store: viewStateStore }, () => h(App)) })
+const selectionStore = markRaw(createSelectionStore())
+const Root = defineComponent({
+  setup: () => () => h(SelectionProvider, { store: selectionStore }, () => h(WidgetViewStateProvider, { store: viewStateStore }, () => h(App))),
+})
 createApp(Root).mount('#app')
