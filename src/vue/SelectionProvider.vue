c387a77d65294f43854860f0e1bd1b776f84fd40
Kevni92 <mail@kevinkrone.de>
2026-08-19 11:47:22 +0200

    Add selection provider

---
+++ src/vue/SelectionProvider.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { toRaw } from 'vue'
+import type { SelectionStore } from '../core/selection'
+import { provideSelectionStore } from './selection-context'
+
+const props = defineProps<{ store: SelectionStore }>()
+provideSelectionStore(toRaw(props.store))
+</script>
+
+<template><slot /></template>
