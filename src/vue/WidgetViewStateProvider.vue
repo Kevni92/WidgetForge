1e621a77c56dffbe51c0b51fdb9bc9081cb67456
Kevni92 <mail@kevinkrone.de>
2026-08-19 11:27:53 +0200

    Add widget view state provider

---
+++ src/vue/WidgetViewStateProvider.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { toRaw } from 'vue'
+import type { WidgetViewStateStore } from '../core/widget-view-state'
+import { provideWidgetViewState } from './widget-view-state'
+
+interface Props { store: WidgetViewStateStore; scopeId?: string }
+const props = withDefaults(defineProps<Props>(), { scopeId: 'default' })
+provideWidgetViewState(toRaw(props.store), () => props.scopeId)
+</script>
+
+<template><slot /></template>
